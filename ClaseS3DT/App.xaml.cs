f2912a9b6e7fcba80221e7148615f4b4506788d3
master <master@SMLAB408PC22_23>
2022-12-13 19:31:42 -0500

    Agregar archivos de proyecto.

---
+++ ClaseS3DT/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ClaseS3DT
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            // NavigationPage() habilita la navegación entre páginas
+            MainPage = new NavigationPage(new Login());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ClaseS3DT/Login.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ClaseS3DT
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Clicked(object sender, EventArgs e)
+        {
+            string user = "dtamayo";
+            string pass = "12345";
+            if (user == txtUsuario.Text && pass == txtPass.Text)
+            {
+                Navigation.PushAsync(new Registro(user));
+            }
+            else
+            {
+                DisplayAlert("Error", "Usuario y/o Contraseña Incorrectos", "Cerrar");
+            }
+        }
+    }
+}
